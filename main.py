e173939f448e0f963223922313732f5c6e2b24ff
Damián Brizuela <dbrizuela@zoonsuite.com>
2025-12-01 12:02:58 -0300

    refatoring example

--- main.py
+++ main.py
@@ -10,16 +10,34 @@ from fastapi import FastAPI
 from step_3.routers import first_router
 
 HOST = "localhost"
-PORT = 8300
+PORT = 8301
 
 app= FastAPI(title= "FastApi")
-app.include_router(first_router)
+
+# step 1 enrutamiento básico
+@app.get("/")
+def read_root():
+    return {"message": "Hello World from step 1!"}
+
+@app.get("/status")
+def get_status():
+        return {"message": "ok-step-2"}
+    
+# step 2 enrutamiento con POST (se puede comprobar mediante un curl o postman)
+"""
+    curl -X POST http://127.0.0.1:8000/info \
+     -H "Content-Type: application/json"
+ """
+@app.post("/info")
+def get_status():
+    return {"message": "ok-step-2 POST verb"}
+
+# step 3 usando routers (agrupando endpoints)
+
 
 def init_server():
-
-    uvicorn.run("main:app", host=HOST, port=PORT, reload=True)
     print("FastAPI Server running...")
-
+    uvicorn.run("main:app", host=HOST, port=PORT, reload=True)
 
 if __name__ == "__main__":
     init_server()
